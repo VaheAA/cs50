0271fbbe17825bc4f5ea207a5a6804c509d5d0c9
vahea <vahe.abov@gmail.com>
2024-09-12 00:18:47 +0400

    add mario

--- week1/mario.c
+++ week1/mario.c
@@ -1,11 +1,22 @@
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-
-    for(int i = 0; i < 4; i++)
+    int n;
+    do
     {
-        printf("?");
+     n = get_int("Size: ");
     }
-    printf("\n");
+    while (n < 1);
+
+    for (int i = 0; i < n; i++)
+    {
+       for (int j = 0; j < n; j++)
+       {
+         printf("#");
+       }
+       printf("\n");
+    }
+
 }
