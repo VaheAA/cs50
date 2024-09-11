0716b7a66065283b808054be510135875bf1255c
vahea <vahe.abov@gmail.com>
2024-09-11 23:50:48 +0400

    add calculator

--- meow.c
+++ meow.c
@@ -1,13 +1,17 @@
-#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-    int i = 3;
+    // int i = 0;
 
-    while (i > 0)
+    // while (i < 3)
+    // {
+    //     printf("Meow\n");
+    //     i++;
+    // }
+
+    for (int i = 0; i < 3; i++)
     {
         printf("Meow\n");
-        i--;
     }
 }
